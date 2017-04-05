51ee602cadd92647ad75851cd8c8fa7fc65ced53
Vitalie D <vitalie.dev@gmail.com>
2017-04-05 17:46:42 +0300

    Log fingerprint that doesn’t match supplied fingerprint

--- lib/omniauth/strategies/wsfed/xml_security.rb
+++ lib/omniauth/strategies/wsfed/xml_security.rb
@@ -58,7 +58,7 @@ module OmniAuth
             fingerprint = digest_algorithm(cert.signature_algorithm).hexdigest(cert.to_der)
 
             if fingerprint != idp_cert_fingerprint.gsub(/[^a-zA-Z0-9]/,"").downcase
-              return soft ? false : (raise OmniAuth::Strategies::WSFed::ValidationError.new("Fingerprint mismatch"))
+              return soft ? false : (raise OmniAuth::Strategies::WSFed::ValidationError.new("Fingerprint mismatch. Fingerprint: #{fingerprint}"))
             end
 
             validate_doc(base64_cert, soft)
